24a511d289c377a348371eb5d1094087f2b8b6c2
savitsky <mail@savitsky.in>
2012-11-22 08:12:28 +0200

    Initial Commit

---
+++ MagicTabsObjC/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MagicTabsObjC
+//
+//  Created by Cyril Savitsky on 11/22/12.
+//  Copyright (c) 2012 Synobi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
